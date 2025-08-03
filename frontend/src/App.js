5e0f4a11286ceeab95c3dd3e3705e920ddb7b518
LegalDocConverter <deploy@legaldocconverter.com>
2025-08-03 17:07:54 +0000

    auto-commit for 3d3958e2-72be-46ae-a013-d6f30751022f

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
 import DocumentProcessor from "./components/DocumentProcessor";
 import Pricing from "./components/Pricing";
 import Footer from "./components/Footer";
+import SignInModal from "./components/auth/SignInModal";
+import SignUpModal from "./components/auth/SignUpModal";
 import { Toaster } from "./components/ui/toaster";
 
 const Home = () => {
@@ -25,12 +28,16 @@ const Home = () => {
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-      <Toaster />
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+        <SignInModal />
+        <SignUpModal />
+        <Toaster />
+      </AuthProvider>
     </div>
   );
 }
